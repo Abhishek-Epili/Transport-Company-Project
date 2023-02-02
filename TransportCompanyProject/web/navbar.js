b3ffd76a037d156c3d9e2cad68c7945a4667fbf9
Abhishek Epili <115881569+Abhishek-Epili@users.noreply.github.com>
2023-02-02 16:34:23 +0530

    Delete TransportCompanyProject directory

--- TransportCompanyProject/web/navbar.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#nav-placeholder").load("nav.html");
-});
-$(function(){
-    $("#about-placeholder").load("about.html");
-});
-$(function(){
-    $("#contact-placeholder").load("contact.html");
-});
